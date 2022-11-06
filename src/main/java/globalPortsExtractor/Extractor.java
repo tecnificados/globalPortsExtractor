b8dc8a3875b57147255f9e72a0dda902f955a05e
Juan Carlos Ballesteros <jcballesteroshermida@gmail.com>
2022-11-06 18:39:40 +0100

    A bit of order

--- src/main/java/globalPortsExtractor/Extractor.java
+++ src/main/java/globalPortsExtractor/Extractor.java
@@ -12,23 +12,14 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.measure.MetricPrefix;
-import javax.measure.Quantity;
-import javax.measure.quantity.Length;
-
-import org.geotools.geometry.DirectPosition2D;
-import org.geotools.geometry.jts.JTS;
 import org.geotools.referencing.CRS;
 import org.geotools.referencing.GeodeticCalculator;
-import org.geotools.referencing.crs.DefaultGeographicCRS;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.opengis.referencing.FactoryException;
 import org.opengis.referencing.NoSuchAuthorityCodeException;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
-import org.opengis.referencing.operation.MathTransform;
-
 import com.bedatadriven.jackson.datatype.jts.JtsModule;
 import com.bedatadriven.jackson.datatype.jts.parsers.GenericGeometryParser;
 import com.bedatadriven.jackson.datatype.jts.parsers.GeometryParser;
@@ -41,10 +32,6 @@ import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.GeometryFactory;
 import com.vividsolutions.jts.operation.distance.DistanceOp;
 
-import si.uom.SI;
-import tech.units.indriya.ComparableQuantity;
-import tech.units.indriya.quantity.Quantities;
-
 /**
  * @author Juan Carlos Ballesteros (tecnificados.com)
  *
@@ -53,7 +40,7 @@ public class Extractor {
 
 	private static String portsFilePath = "D:\\datos\\globalPorts\\portsWSG84.geojson";
 
-	private static String bigWaterBodiesPath = "D:\\datos\\waterBodies\\body3.geojson";
+	private static String bigWaterBodiesPath = "D:\\datos\\waterBodies\\bigWaterBodyWSG84.geojson";
 
 	/**
 	 * @param args
@@ -63,7 +50,7 @@ public class Extractor {
 	public static void main(String[] args) throws NoSuchAuthorityCodeException, FactoryException {
 
 		List<Geometry> portsList = new ArrayList<Geometry>();
-		List<Geometry> spainPortsList = new ArrayList<Geometry>();
+		
 
 		JSONArray ports = new JSONArray();
 		File dataFile = new File(portsFilePath);
@@ -98,14 +85,7 @@ public class Extractor {
 			Geometry actualGeometry = generateGeometryFromGeojson(customPoint.toJSONString());
 			portsList.add(actualGeometry);
 
-			if (properties.containsKey("country")) {
-				String country = (String) properties.get("country");
-				String portname = (String) properties.get("portname");
-
-				if (country != null && country.equalsIgnoreCase("spain")) {
-						spainPortsList.add(actualGeometry);
-				}
-			}
+			
 
 			if (portTypes.containsKey(portType)) {
 				Integer v = portTypes.get(portType);
@@ -119,41 +99,43 @@ public class Extractor {
 		System.out.println("There are " + portTypes.size() + " port types");
 		System.out.println(portTypes);
 
+		
 		Geometry geoWater = generateGeometryFromGeojson(new File(bigWaterBodiesPath));
-		Geometry p1 = spainPortsList.get(0);
-		Geometry p2 = spainPortsList.get(1);
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.printf("Distance 1:  %f\n", p1.distance(p2));
+		
 
 		String EPSG4326 = "GEOGCS[\"WGS 84\",DATUM[\"WGS_1984\",SPHEROID[\"WGS 84\",6378137,298.257223563,AUTHORITY[\"EPSG\",\"7030\"]],AUTHORITY[\"EPSG\",\"6326\"]],PRIMEM[\"Greenwich\",0,AUTHORITY[\"EPSG\",\"8901\"]],UNIT[\"degree\",0.01745329251994328,AUTHORITY[\"EPSG\",\"9122\"]],AUTHORITY[\"EPSG\",\"4326\"]]";
 	    CoordinateReferenceSystem crs = CRS.parseWKT(EPSG4326);
-		GeodeticCalculator calculator = new GeodeticCalculator(crs);
+		GeodeticCalculator calculator = new GeodeticCalculator(crs);/*
 		calculator.setStartingGeographicPoint(p1.getCentroid().getX(), p1.getCentroid().getY());
 		calculator.setDestinationGeographicPoint(p2.getCentroid().getX(), p2.getCentroid().getY());		
-		double distanceInMeters = calculator.getOrthodromicDistance();	
+		double distanceInMeters = calculator.getOrthodromicDistance();	*/
 		
-	
-		for (int i = 0; i < spainPortsList.size(); i++) {
+		int counter=0;
+		for (int i = 0; i < portsList.size(); i++) {
 			Geometry portGeometry = portsList.get(i);
-			double distance3 = geoWater.distance(portGeometry);
-			System.out.println(distance3);
-			
+			double distance3 = geoWater.distance(portGeometry);				
 			Coordinate[] nearestPoints = DistanceOp.nearestPoints(geoWater, portGeometry);
-					
-			System.out.println(nearestPoints);
-			
 			Coordinate c1 = nearestPoints[0];
-			Coordinate c2 = nearestPoints[1];
-			
+			Coordinate c2 = nearestPoints[1];			
 			
 			calculator.setStartingGeographicPoint(c1.x, c1.y);
 			calculator.setDestinationGeographicPoint(c2.x, c2.y);		
-			double distanceInMeters2 = calculator.getOrthodromicDistance();	
+			double distanceInMeters = calculator.getOrthodromicDistance();	
+			double distanceInKM=distanceInMeters/1000;
 			
-			System.out.println(distanceInMeters2+" metros");
+			
+			if (distanceInKM>5) {
+				JSONObject actualPort=(JSONObject) ports.get(i);
+				System.out.println(actualPort.toJSONString());
+				System.out.println(distanceInKM+" KM");
+			}else {
+				counter++;
+			}
+			
+			
 			
 		}
+		System.out.println(counter);
 		System.out.println("End");
 	}
 
